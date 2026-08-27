11205fc53bdb5422be353633fa6f4ce66fa0813a
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-08-27 12:47:20 +0200

    Add testable ZCL value normalization module

---
+++ main/gateway_zcl_value.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "gateway_events.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint16_t gateway_zcl_attr_size(uint8_t type, const void *value);
+
+bool gateway_zcl_normalize(uint16_t cluster,
+                           uint16_t attribute,
+                           uint8_t type,
+                           const void *value,
+                           gateway_measurement_kind_t *kind,
+                           gateway_unit_t *unit,
+                           double *number);
+
+#ifdef __cplusplus
+}
+#endif
